17716fe824839b79693a4c4801caec08207e16b8
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-11-09 18:11:11 +0100

    setting "Access-Control-Allow-Credentials"

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -88,7 +88,8 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*'
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify(token),
       };
