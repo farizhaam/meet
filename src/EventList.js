285281736ed1a7d84ac14e24ee21e9dd9ffc4fdd
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-11-13 23:09:25 +0100

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
+import Event from './Event';
 
 class EventList extends Component {
-    render(){
+    render() {
+        const {events} = this.props;
         return (
-            <div>
-                <ul className="EventList">
-                    
-                </ul>
-            </div>
+            <ul className="EventList">
+                {events.map(event =>
+                    <li key={event.id}>
+                        <Event event={event} />
+                    </li>
+                )}
+            </ul>
         );
     }
 }
