a8faa996b170c87e627457fe402059411921fbd9
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-11-13 16:27:27 +0100

    Create EventList.js

---
+++ src/EventList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class EventList extends Component {
+    render(){
+        return (
+            <div>
+                <ul className="EventList">
+                    
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default EventList;
