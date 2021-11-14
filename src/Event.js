608f1f8317e54430421a1c12a4ff45004f75d675
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-11-14 02:00:53 +0100

    Update Event.js

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,40 @@
-import React, {Component} from "react";
+import React, {Component, } from "react";
+import { useState, useEffect } from React;
+import { Button} from 'react-bootstrap';
+import {Collapse} from 'react-bootstrap';
 
 class Event extends Component {
+    state = {
+        collapsed: true,
+    };
+    
+    handleItemClick = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
+    const [open, setOpen] = useState(false);
     render() {
-        return <div></div>;
+        const { events } = this.props;
+        const { collapsed } = this.state;
+        return (
+            <div className="Event">
+                <h2 className="summary">{events.summary}</h2>
+                <p className="start-date">
+                    {events.start.dateTime} ({events.start.timeZone})
+                </p>
+                <p className="end-date">
+                    {events.end.dateTime} ({events.end.timeZone})
+                </p>
+                <p className="location">
+                    {events.location}
+                </p>
+                <Button variant="primary" className="details" onClick={this.handleItemClick}>
+                    Toggle details
+                </Button>
+            </div>
+        );
     }
 }
 
