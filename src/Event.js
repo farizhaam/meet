df941b05d38f1ee19586de7e58859ba7eb29956c
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-11-15 23:21:17 +0100

    changing Collapse mechanism

--- src/Event.js
+++ src/Event.js
@@ -1,39 +1,56 @@
-import React, {Component, } from "react";
-import { useState, useEffect } from React;
+import React, {Component} from "react";
 import { Button} from 'react-bootstrap';
 import {Collapse} from 'react-bootstrap';
 
 class Event extends Component {
-    state = {
-        collapsed: true,
-    };
     
-    handleItemClick = () => {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            collapsed: true
+        };
+    
+        this.handleClick = this.handleClick.bind(this);
+    }
+    //   state = {
+    //     collapsed: true,
+    // };
+
+    handleClick(e) {
+        // e.preventDefault();
+
         this.setState({
             collapsed: !this.state.collapsed,
         });
     };
 
-    const [open, setOpen] = useState(false);
     render() {
-        const { events } = this.props;
-        const { collapsed } = this.state;
-        return (
-            <div className="Event">
-                <h2 className="summary">{events.summary}</h2>
-                <p className="start-date">
-                    {events.start.dateTime} ({events.start.timeZone})
-                </p>
-                <p className="end-date">
-                    {events.end.dateTime} ({events.end.timeZone})
-                </p>
-                <p className="location">
-                    {events.location}
-                </p>
-                <Button variant="primary" className="details" onClick={this.handleItemClick}>
-                    Toggle details
-                </Button>
-            </div>
+    const { event } = this.props;
+    return (
+        <div className="event">
+            <h2 className="summary">{event.summary}</h2>
+            <p className="start-date">
+                {event.start.dateTime} ({event.start.timeZone})
+            </p><p className="end-date">
+                {event.end.dateTime} ({event.end.timeZone})
+            </p>
+            <p className="location">
+                {event.location}
+            </p>
+
+            <Button variant="primary" className="toggle-details" onClick={this.handleClick}>
+                Toggle details
+            </Button>
+            <Collapse in={!this.state.collapsed}>
+                <div className="info-details">
+                    <p className="event-description">
+                        {event.description}
+                    </p>
+                </div>
+            </Collapse>
+        </div>
+    
         );
     }
 }
