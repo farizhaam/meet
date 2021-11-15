5ba020708dda4c2b75cc344a10453eb8875105a1
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-11-15 23:21:46 +0100

    adding 8 tests and all passed

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -7,12 +7,46 @@ describe('<Event /> component', () => {
     let EventWrapper;
     
     beforeAll(() => {
-        EventWrapper = shallow(<Event/>);
+        EventWrapper = shallow(<Event event={mockData[0]}/>);
     });
 
 
-    test('verify wheteher all necessary elements have been rendered', () => {
-        const EventWrapper = shallow(<Event events={mockData} />);
-        expect(EventWrapper.find(Event)).toHaveLength(mockData.length);
+    test('render element collapsed by default', () => {
+        EventWrapper.setState({
+            collapsed: true
+        })
+        expect(EventWrapper.state("collapsed")).toBe(true);
+    });
+
+    test('render event summary correctly', () => {
+        expect(EventWrapper.find(".summary")).toHaveLength(1);
+    });
+
+    test('render event start date correctly', () => {
+        expect(EventWrapper.find(".start-date")).toHaveLength(1);
+    });
+
+    test('render event end date correctly', () => {
+        expect(EventWrapper.find(".end-date")).toHaveLength(1);
+    });
+
+    test('render event location correctly', () => {
+        expect(EventWrapper.find(".location")).toHaveLength(1);
+    });
+
+    test('render toggle details button correctly', () => {
+        expect(EventWrapper.find(".toggle-details")).toHaveLength(1);
+    });
+
+    test('render change the state of toggle details button when clicked', () => {
+        EventWrapper.find('.toggle-details').simulate('click');
+        expect(EventWrapper.state("collapsed")).toBe(false);
+    });
+
+    test('render details correctly when the collapsed state is false', () => {
+        EventWrapper.setState({
+            collapsed: false,
+        });
+        expect(EventWrapper.find(".info-details")).toHaveLength(1);
     });
 });
