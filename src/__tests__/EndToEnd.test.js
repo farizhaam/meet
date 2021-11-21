c125bb2e154ce7a09caef1c7c7054e61373eb876
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-11-21 22:18:32 +0100

    error: refused connection with localhost

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,17 +1,30 @@
 import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
-    test('An event element is collapsed by default', async () => {
+    
+    let browser;
+    let page;
 
-        const browser = await puppeteer.launch();
-
-        const page = await browser.newPage();
+    beforeAll(async () => {
+        jest.setTimeout(30000);
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
         await page.goto('http://localhost:3000/');
-    
         await page.waitForSelector('.event');
-    
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
+
+    test('An event element is collapsed by default', async () => {
         const eventDetails = await page.$('.event .info-details');
         expect(eventDetails).toBeNull();
-        browser.close();
+    });
+
+    test('User can expand an event to see its details', async () => {
+        await page.click('.event .toggle-details');
+        const eventDetails = await page.$('.event .info-details');
+        expect(eventDetails).toBeDefined();
     });
 });
