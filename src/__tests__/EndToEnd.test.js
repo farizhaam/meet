610faf3deb58ab6e693bf6454c40bc8bf6f21a1d
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-11-21 22:53:01 +0100

    setting headless browser to false and slowMo

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -7,7 +7,11 @@ describe('show/hide an event details', () => {
 
     beforeAll(async () => {
         jest.setTimeout(30000);
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 250,
+            ignoreDefaultArgs: ['--disable-extensions']
+        });
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
         await page.waitForSelector('.event');
@@ -26,5 +30,11 @@ describe('show/hide an event details', () => {
         await page.click('.event .toggle-details');
         const eventDetails = await page.$('.event .info-details');
         expect(eventDetails).toBeDefined();
+    });
+
+    test('User can collapse an event to hide its details', async () => {
+        await page.click('.event .toggle-details');
+        const eventDetails = await page.$('.event .info-details');
+        expect(eventDetails).toBeNull();
     });
 });
